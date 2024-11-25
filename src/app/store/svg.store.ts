643c32973e60303c10f3cb2270cf7de0489e1a88
Arshdeep Singh <arsh199820@gmail.com>
2024-11-25 09:09:49 +0530

    feat: page not found page

--- src/app/store/svg.store.ts
+++ src/app/store/svg.store.ts
@@ -32,4 +32,10 @@ export const svgIcons = {
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
     </svg>
     `,
+
+  undo: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9h13a5 5 0 0 1 0 10H7M3 9l4-4M3 9l4 4"/>
+    </svg>
+    `,
 };
