95e774888769c0d1a4a3bd70d5ad8d892a5a128b
Arshdeep Singh <arsh199820@gmail.com>
2025-02-24 10:30:40 +0530

    feat: add standalone Spinner component and integrate into app and Finasync modules

--- src/app/modules/finasync/finasync.module.ts
+++ src/app/modules/finasync/finasync.module.ts
@@ -12,7 +12,7 @@ import { FormComponent } from './components/bank-accounts/form/form.component';
 import { AccountsTableComponent } from './components/bank-accounts/accounts-table/accounts-table.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SharedComponentsModule } from 'src/app/modules/shared-components/shared-components.module';
+import { SpinnerComponent } from 'src/app/standalone-components/spinner/spinner.component';
 
 @NgModule({
   imports: [
@@ -22,7 +22,9 @@ import { SharedComponentsModule } from 'src/app/modules/shared-components/shared
     FormsModule,
     NgSelectModule,
     FontAwesomeModule,
-    SharedComponentsModule,
+
+    // standalone
+    SpinnerComponent,
   ],
   declarations: [
     FinasyncDashboardComponent,
